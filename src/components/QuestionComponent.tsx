9ccae0a4b8a12b2a1a145673e1b6e6aba2da2c7b
Edaurd Kosyan <edkosyan2000@gmail.com>
2026-02-05 17:19:55 +0400

    fix bug

--- src/components/QuestionComponent.tsx
+++ src/components/QuestionComponent.tsx
@@ -2,8 +2,7 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import type { IQuestion } from "../types/IQuestion";
 import s from "./QuestionComponent.module.css";
 //import all from 'react-syntax-highlighter/dist/esm/styles/prism';
-import {dark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
+import { monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 interface Props {
   question: IQuestion;
@@ -17,14 +16,18 @@ export const QuestionComponent = ({ question, showAnswer }: Props) => {
       <h2 className={s.title}>
         <span>{question.id! + 1}.</span> {question.title}
       </h2>
-      <p className={s.answer} style={{ opacity: showAnswer ? 1 : 0 }}>
-        {question.answer}
-      </p>
-      {question.code && (
-        <SyntaxHighlighter language="TypeScript" style={dark} customStyle={{fontSize: "12px"}}>
-          {question.code}
-        </SyntaxHighlighter>
-      )}
+      <div style={{ opacity: showAnswer ? 1 : 0 }}>
+        <p className={s.answer}>{question.answer}</p>
+        {question.code && (
+          <SyntaxHighlighter
+            language="typescript"
+            style={monokai}
+            customStyle={{ fontSize: "12px" }}
+          >
+            {question.code}
+          </SyntaxHighlighter>
+        )}
+      </div>
     </div>
   );
 };

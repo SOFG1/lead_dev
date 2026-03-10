a443a48eedad8ce89cd2ebf4a7767a5d1e1faad4
Edaurd Kosyan <edkosyan2000@gmail.com>
2026-03-10 15:28:37 +0400

    add ci/cd

--- src/questions.ts
+++ src/questions.ts
@@ -982,4 +982,27 @@ In TypeScript we can define in a single file that a variable which exists global
     declare const API_URL: string; `,
     tag: "4 TypeScript",
   },
+  {
+    title: "CI/CD",
+    answer: `
+Continuous Integration / Continuous delivery - is a devops method for frequently build, test and deploy code changes
+
+CI - Automated tools which immediately build and test the code
+CD - Tools which build, test and prepare code for production
+Continuous Deployment - Advanced form of deployment when code automatically deployed to production
+     `,
+    tag: "5 CI/CD and Devtools",
+  },
+  {
+    title: "Yarn and NPM pros and cons",
+    answer: `
+Yarn is more performant than NPM
+Yarn has monorepo support
+Yarn is not user friendly for beginners
+
+NPM is very popular and easy for beginners, also has a large community
+NPM is less performant than yarn
+     `,
+    tag: "5 CI/CD and Devtools",
+  },
 ];

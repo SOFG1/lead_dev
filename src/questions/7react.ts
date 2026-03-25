a9461d41e38939f5776ff5dc07b868088b850f4f
Edaurd Kosyan <edkosyan2000@gmail.com>
2026-03-25 14:07:09 +0400

    fix bug

--- src/questions/7react.ts
+++ src/questions/7react.ts
@@ -4,13 +4,6 @@ export const questions7: Omit<IQuestion, "id">[] = [
   {
     title: "React fiber",
     answer: `
-    React Fiber is React's core reconciliation algorithm, introduced in React 16 to enable incremental rendering and improve performance
-    `,
-    tag: "7 React",
-  },
-  {
-    title: "React fiber",
-    answer: `
 React Fiber is React's core reconciliation algorithm, introduced in React 16 to enable incremental rendering and improve performance
     `,
     tag: "7 React",

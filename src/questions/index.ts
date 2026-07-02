10eb7f918f2dc0e954e1e816a05e52cd9aced285
Edaurd Kosyan <edkosyan2000@gmail.com>
2026-07-02 13:19:43 +0400

    add 13 rendering strategies

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -1,33 +1,29 @@
-import { questions1 } from './1htmlcss';
-import { questions2 } from './2javascript';
-import { questions3 } from './3regexp';
-import { questions4 } from './4typescript';
-import { questions5 } from './5cicd';
-import { questions6 } from './6browserAPIs';
-import { questions7 } from './7react';
-import { questions8 } from './8softwareDesign';
-import { questions9 } from './9designPatterns';
-import { questions10 } from './10webPerformance';
-import { questions11 } from './11security';
-import { questions12 } from './12communicationProtocols';
-
-
-
-
-
-
+import { questions1 } from "./1htmlcss";
+import { questions2 } from "./2javascript";
+import { questions3 } from "./3regexp";
+import { questions4 } from "./4typescript";
+import { questions5 } from "./5cicd";
+import { questions6 } from "./6browserAPIs";
+import { questions7 } from "./7react";
+import { questions8 } from "./8softwareDesign";
+import { questions9 } from "./9designPatterns";
+import { questions10 } from "./10webPerformance";
+import { questions11 } from "./11security";
+import { questions12 } from "./12communicationProtocols";
+import { questions13 } from "./13renderingStrategies";
 
 export const allQuestions = [
-    ...questions1,
-    ...questions2,
-    ...questions3,
-    ...questions4,
-    ...questions5,
-    ...questions6,
-    ...questions7,
-    ...questions8,
-    ...questions9,
-    ...questions10,
-    ...questions11,
-    ...questions12
-]
+  ...questions1,
+  ...questions2,
+  ...questions3,
+  ...questions4,
+  ...questions5,
+  ...questions6,
+  ...questions7,
+  ...questions8,
+  ...questions9,
+  ...questions10,
+  ...questions11,
+  ...questions12,
+  ...questions13
+];

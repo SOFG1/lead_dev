86fa1da83e8fd4ba1b3128fbd98be3cba4c28911
Eduard Kosyan <edkosyan2000@gmail.com>
2026-08-25 11:57:01 +0400

    setup code coverage metrics

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lead_dev/', // Add this line
+  base: '/lead_dev/', // Deployed base url
   test: {
     exclude: [...configDefaults.exclude, 'e2e_tests'],
+    coverage: {
+      provider: 'v8',
+    },
   },
   css: {
     modules: {
